375b244f437d4ea2ea02e9e65ca5d22df8b5f6d7
ckie <git-525ff67@ckie.dev>
2023-11-02 19:39:34 +0200

    rust wip

--- flake.nix
+++ flake.nix
@@ -7,12 +7,20 @@
       url = "github:ckiee/flake-compat/add-overrideInputs";
       flake = false;
     };
+    esp-dev = {
+      url = "github:thiskappaisgrey/nixpkgs-esp-dev-rust";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs, ... }:
+  outputs = { self, flake-utils, naersk, nixpkgs, esp-dev, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = (import nixpkgs) { inherit system; };
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [ esp-dev.overlay ];
+        };
+        inherit (pkgs) lib;
 
         naersk' = pkgs.callPackage naersk { };
 
@@ -54,7 +62,60 @@
         defaultPackage = packages.ledc;
 
         # There's a shell.nix in ./ledc for now
-        # devShell =
-        #  pkgs.mkShell { nativeBuildInputs = with pkgs; [ rustc cargo rust-analyzer ]; };
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            # rustc
+            # cargo
+            clippy
+            rust-analyzer
+gcc-xtensa-esp32-elf-bin
+rust-xtensa
+          ];
+
+          buildInputs = with pkgs; [
+            udev
+            cmake
+            fontconfig
+            openssl # cargo-generate
+            openocd-esp32-bin
+
+            # esp-idf depends
+            git
+            wget
+            gnumake
+
+            flex
+            bison
+            gperf
+            pkg-config
+
+            cmake
+            ninja
+
+            ncurses5
+
+            python3
+            python3Packages.pip
+            python3Packages.virtualenv
+          ];
+
+          LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+            libGL
+            libxkbcommon
+            wayland
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+            pkgs.libxml2
+            pkgs.zlib
+            pkgs.stdenv.cc.cc.lib
+          ]);
+
+          ESP_IDF_VERSION = "v4.4";
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          ESP_IDF_SYS_ROOT_CRATE = "ledfw";
+        };
       });
 }
